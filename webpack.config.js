fdcbcce3cb337ad56869fbdda6f929010d6de0bb
Dan Gilkerson <contact@dangilkerson.com>
2016-04-28 00:11:00 -0400

    Use webpack instead of browserify

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var WrapperPlugin = require('wrapper-webpack-plugin');
+
+module.exports = {
+    entry: "./src/glance-selector",
+    output: {
+        path: './lib',
+        filename: 'glance-selector.js'
+    },
+    plugins: [
+        new WrapperPlugin({
+            header: 'module.exports = function() {',
+            footer: '}'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            mangle: false
+        })
+    ],
+    module: {
+        loaders: [
+            {test: /.js/, loaders: ['babel-loader']}
+        ]
+    }
+};
