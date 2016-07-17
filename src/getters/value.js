4cf10a12f36cbb94f17437a7c666187330f90f55
Dan Gilkerson <contact@dangilkerson.com>
2016-07-17 14:02:04 -0700

    Remove dead code now that we're using a common module.

--- src/getters/value.js
+++
@@ -1,6 +0,0 @@
-import {getValueFromClient} from '../utils/client';
-
-export default function(g, selector) {
-    selector = selector.replace(/:value$/, "");
-    return g.find(selector).then((wdioSelector)=> g.browser.getValue(wdioSelector));
-}
