244571298c23c8c601d8d572c62ba59c89b1d1ed
Dan Gilkerson <contact@dangilkerson.com>
2016-08-19 10:58:09 -0700

    Handle stale element error using a timeout

--- src/glance.js
+++ src/glance.js
@@ -11,16 +11,28 @@ export default class Glance extends GlanceCommon {
         config.browserExecute = (func, ...args) => {
             let callback = args.pop();
 
-            return this.browser.executeAsync(function () {
-                var args = Array.prototype.slice.call(arguments);
-                var func = args.shift();
-                var done = args.pop();
-                args.push(function (err, result) {
-                    done(result);
-                });
+            return new Promise((resolve, reject)=> {
+                var timeout = setTimeout(function () {
+                    reject("Browser Execute Timeo");
+                }, 2000);
 
-                eval("(" + func + ")").apply(null, args);
-            }, func.toString(), ...args).then(res => callback(null, res), callback);
+                return this.browser.executeAsync(function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    var func = args.shift();
+                    var done = args.pop();
+                    args.push(function (err, result) {
+                        done(result);
+                    });
+
+                    eval("(" + func + ")").apply(null, args);
+                }, func.toString(), ...args).then(result => {
+                    clearTimeout(timeout);
+                    return resolve(result)
+                }, error => {
+                    clearTimeout(timeout);
+                    return reject(error)
+                });
+            }).then(res => callback(null, res), callback);
         }
 
         super(config);
